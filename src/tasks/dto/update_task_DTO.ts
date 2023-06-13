2ff6e5870e81563349e155e05f29ab5491a14ad7
Sergiok12 <sergioabd460@gmail.com>
2023-06-13 12:39:48 -0600

    Added validators with ValidatorClass Package

--- src/tasks/dto/update_task_DTO.ts
+++ src/tasks/dto/update_task_DTO.ts
@@ -1,7 +1,17 @@
+import { IsIn, IsOptional, IsString } from "class-validator";
 import { Taskstatus } from "../task.entity"
 
 export class UpdateTaskDto{
+    @IsString()
+    @IsOptional()
     title?: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsIn([Taskstatus.PENDING, Taskstatus.IN_PROGRESS,Taskstatus.FINISH])
     status?: Taskstatus;
 }
